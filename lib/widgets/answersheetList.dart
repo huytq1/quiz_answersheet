f325064882d90fb48ace2b3073915062cb5fe3d4
Panda Chan <huytq1@gmail.com>
2018-12-27 13:06:51 +0700

    add answer widget

---
+++ lib/widgets/answersheetList.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AnswerSheetListWidget extends StatefulWidget {
+  @override
+  _AnswerSheetListStateWidget createState() => _AnswerSheetListStateWidget();
+}
+
+class _AnswerSheetListStateWidget extends State<AnswerSheetListWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
